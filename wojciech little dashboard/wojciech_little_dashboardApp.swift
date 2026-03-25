94482121dad7bfa268839d983730fd1f61dad5b5
Wojciech Duda <wojciechduda03@gmail.com>
2026-03-25 15:47:06 +0100

    Initial Commit

---
+++ wojciech little dashboard/wojciech_little_dashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  wojciech_little_dashboardApp.swift
+//  wojciech little dashboard
+//
+//  Created by Wojciech Duda on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct wojciech_little_dashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
