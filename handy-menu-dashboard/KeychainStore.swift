1a8252c4da99c03114528f3f5995240ec57fea70
Wojciech Duda <wojciech.duda@hyland.com>
2026-06-29 16:46:00 +0200

    fix: logout on quitting application

--- handy-menu-dashboard/KeychainStore.swift
+++ handy-menu-dashboard/KeychainStore.swift
@@ -5,44 +5,29 @@ struct KeychainStore: KeychainStoring {
     private let serviceName = "handy-menu-dashboard"
 
     func save(key: KeychainKey, data: Data) {
-        var query = baseQuery(for: key)
-        query[kSecUseDataProtectionKeychain as String] = true
+        let query = baseQuery(for: key)
 
-        let attributes: [String: Any] = [
-            kSecValueData as String: data,
-            kSecAttrAccessible as String: kSecAttrAccessibleAfterFirstUnlock
-        ]
-
-        let updateStatus = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        let updateStatus = SecItemUpdate(query as CFDictionary, [kSecValueData as String: data] as CFDictionary)
         if updateStatus == errSecItemNotFound {
             var addQuery = query
             addQuery[kSecValueData as String] = data
-            addQuery[kSecAttrAccessible as String] = kSecAttrAccessibleAfterFirstUnlock
             SecItemAdd(addQuery as CFDictionary, nil)
         }
-
-        deleteLegacyItem(for: key)
     }
 
     func load(key: KeychainKey) -> Data? {
-        if let data = loadFromDataProtectionKeychain(key: key) {
-            return data
-        }
+        var query = baseQuery(for: key)
+        query[kSecReturnData as String] = true
+        query[kSecMatchLimit as String] = kSecMatchLimitOne
 
-        if let legacyData = loadLegacyItem(key: key) {
-            save(key: key, data: legacyData)
-            return legacyData
-        }
-
-        return nil
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        guard status == errSecSuccess else { return nil }
+        return result as? Data
     }
 
     func delete(key: KeychainKey) {
-        deleteLegacyItem(for: key)
-
-        var query = baseQuery(for: key)
-        query[kSecUseDataProtectionKeychain as String] = true
-        SecItemDelete(query as CFDictionary)
+        SecItemDelete(baseQuery(for: key) as CFDictionary)
     }
 
     private func baseQuery(for key: KeychainKey) -> [String: Any] {
@@ -51,35 +36,5 @@ struct KeychainStore: KeychainStoring {
             kSecAttrService as String: serviceName,
             kSecAttrAccount as String: key.rawValue
         ]
-    }
-
-    private func loadFromDataProtectionKeychain(key: KeychainKey) -> Data? {
-        var query = baseQuery(for: key)
-        query[kSecReturnData as String] = true
-        query[kSecMatchLimit as String] = kSecMatchLimitOne
-        query[kSecUseDataProtectionKeychain as String] = true
-        query[kSecUseAuthenticationUI as String] = kSecUseAuthenticationUIFail
-
-        var result: AnyObject?
-        let status = SecItemCopyMatching(query as CFDictionary, &result)
-        guard status == errSecSuccess else { return nil }
-        return result as? Data
-    }
-
-    private func loadLegacyItem(key: KeychainKey) -> Data? {
-        var query = baseQuery(for: key)
-        query[kSecReturnData as String] = true
-        query[kSecMatchLimit as String] = kSecMatchLimitOne
-        query[kSecUseAuthenticationUI as String] = kSecUseAuthenticationUIFail
-
-        var result: AnyObject?
-        let status = SecItemCopyMatching(query as CFDictionary, &result)
-        guard status == errSecSuccess else { return nil }
-        return result as? Data
-    }
-
-    private func deleteLegacyItem(for key: KeychainKey) {
-        let query = baseQuery(for: key)
-        SecItemDelete(query as CFDictionary)
     }
 }
